3c9cf899ee6a78e5afbb88867bde1a18c87cbe2a
Manthan Singh <manthansingh27525@gmail.com>
2026-03-31 12:43:13 +0530

    Updated api

--- api/main.py
+++ api/main.py
@@ -52,6 +52,7 @@ app = FastAPI(title="AutoDML API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
